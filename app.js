3e2ac9b5931d28ddaa75575a59f219e84f464720
Hozene <jedrek.kusnierz@gmail.com>
2025-01-28 03:50:45 +0100

    Create results page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const { startQuiz } = require("./src/quizController");
+const quizController = require("./src/quizController");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
@@ -18,6 +20,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/quiz", startQuiz); // quiz controller
+
+app.post("/results", quizController.submitQuiz);
 
 // start server
 app.listen(PORT, () => {
--- src/quizController.js
+++ src/quizController.js
@@ -15,4 +15,25 @@ const startQuiz = async (req, res) => {
     }
 };
 
-module.exports = { startQuiz };
+const submitQuiz = (req, res) => {
+    const userAnswers = [];
+    const correctAnswers = [];
+    const totalQuestions = Object.keys(req.body).length / 2; // Half of the inputs are user answers, half are correct answers
+
+    for (let i = 0; i < totalQuestions; i++) {
+        userAnswers.push(req.body[`answer${i}`]); // Extract user answers
+        correctAnswers.push(req.body[`correctAnswer${i}`]); // Extract correct answers
+    }
+
+    // Calculate the score
+    let score = 0;
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score++;
+        }
+    });
+
+    res.render("results", { score, totalQuestions }); // Render the results page
+};
+
+module.exports = { startQuiz, submitQuiz };
